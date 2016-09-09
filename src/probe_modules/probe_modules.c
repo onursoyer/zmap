cb83b01aa228a87fb3fd2ea2fa30979bce413716
onursoyer <onrsyr@gmail.com>
2016-09-09 16:50:17 +0900

    packet sizes test

--- src/probe_modules/probe_modules.c
+++ src/probe_modules/probe_modules.c
@@ -75,9 +75,9 @@ void fs_add_ip_fields(fieldset_t *fs, struct ip *ip) {
     // [MOBI]
     fs_add_uint64(fs, "ip_hl", (uint64_t)ip->ip_hl);
     fs_add_uint64(fs, "ip_v", (uint64_t)ip->ip_v);
-    fs_add_uint64(fs, "ip_tos", (uint64_t)ip->ip_tos,);
+    fs_add_uint64(fs, "ip_tos", (uint64_t)ip->ip_tos);
     fs_add_uint64(fs, "ip_len", (uint64_t) ip->ip_len);
-    fs_add_string(fs, "ip_p", (uint64_t)ip->ip_p,);
+    fs_add_string(fs, "ip_p", (uint64_t)ip->ip_p);
     fs_add_uint64(fs, "ip_sum", (uint64_t) ip->ip_sum);
 
 }
