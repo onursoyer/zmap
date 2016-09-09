6e9ce8b4b1492fc6b9cc58048034e38053bd681e
onursoyer <onrsyr@gmail.com>
2016-09-09 17:15:30 +0900

    packet sizes test

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -135,7 +135,7 @@ void synscan_process_packet(const u_char *packet,
     fs_add_uint64(fs, "th_off", tcp->th_off);
     printf("%d\n", len);
 
-    char *option = (char *) (packet + (char *) ip_hdr + 4 * ip_hdr->ip_hl);
+    char *option = (char *) (tcphdr + sizeof(tcphdr));
     char option_kind = option[0];
     char option_length = (option + 1)[0];
     char *option_variable = (char *) malloc((int)ntohs(tcp->th_off) - 2);
