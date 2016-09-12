f8c11d5716bfcc3a5f8895fb7032c73d3b4ebcb2
onursoyer <onrsyr@gmail.com>
2016-09-12 10:04:41 +0900

    test

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -165,11 +165,13 @@ void synscan_process_packet(const u_char *packet,
         }
         if( _opt->kind == 2 /* MSS */ ) {
             mss = ntohs((uint16_t)*(opt + sizeof(opt)));
+            printf("mss: %d \n", mss);
+
         }
         opt += _opt->size;
     }
 
-    printf("mss: %d \n", mss);
+
 
 
     if (tcp->th_flags & TH_RST) { // RST packet
