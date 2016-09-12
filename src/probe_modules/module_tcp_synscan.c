9c4c17c50616795563225014c488753538148412
onursoyer <onrsyr@gmail.com>
2016-09-12 10:41:27 +0900

    test

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -164,11 +164,13 @@ void synscan_process_packet(const u_char *packet,
             continue;
         }
         if( _opt->kind == 2 /* MSS */ ) {
-            mss = ntohs((uint16_t)*(opt + sizeof(opt)));
+//            mss = ntohs((uint16_t)*(opt + sizeof(opt)));
+            mss = ((*(opt + sizeof(*_opt)))) << 8 + *(opt + sizeof(*_opt) + 1);
             if(mss != 0)
                 printf("==================> mss: %d \n", mss);
         }
         opt += _opt->size;
+        printf("opt->_size: %d\n",opt->_size);
     }
 
 
