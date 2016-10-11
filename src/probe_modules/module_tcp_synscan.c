67041c9287ba2a1aa22a8f7699f343e71420cf78
onursoyer <onrsyr@gmail.com>
2016-10-11 17:50:47 +0900

    bug fix test

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -151,26 +151,29 @@ void synscan_process_packet(const u_char *packet,
     // MSS Parsing
     uint16_t mss = 0;
     printf("[TEST] 111111 \n");
-    uint8_t* opt = (uint8_t*) (packet + 4 * ip_hdr->ip_hl + sizeof(struct ether_header) + sizeof(struct tcphdr));
-    printf("[TEST] 222222 \n");
-    while( *opt != 0 ) {
-        printf("[TEST] 33333333 \n");
-        tcp_option_t* _opt = (tcp_option_t*)opt;
-        if( _opt->kind == 1 /* NOP */ ) {
-            ++opt;  // NOP is one byte;
-            printf("[TEST] 4444444 \n");
-            continue;
-        }
-        if( _opt->kind == 2 /* MSS */ ) {
-            mss = ((*(opt + (sizeof(*_opt)))) << 8) + *(opt + sizeof(*_opt) + 1);
-        }
-        opt += _opt->size;
-        printf("[TEST] 5555555 \n");
-        if(_opt->size == 0)
-            break;
-    }
+    unsigned char *tmp = tcp;
 
-    if(mss != 0)
+    if (tcp->doff > 5) {
+        uint8_t *opt = (uint8_t * )(tmp + sizeof(struct tcphdr));
+        printf("[TEST] 222222 \n");
+        while (*opt != 0) {
+            printf("[TEST] 33333333 \n");
+            tcp_option_t *_opt = (tcp_option_t *) opt;
+            if (_opt->kind == 1 /* NOP */ ) {
+                ++opt;  // NOP is one byte;
+                printf("[TEST] 4444444 \n");
+                continue;
+            }
+            if (_opt->kind == 2 /* MSS */ ) {
+                mss = ((*(opt + (sizeof(*_opt)))) << 8) + *(opt + sizeof(*_opt) + 1);
+            }
+            opt += _opt->size;
+            printf("[TEST] 5555555 \n");
+            if (_opt->size == 0)
+                break;
+        }
+    }
+    if (mss != 0)
         fs_add_uint64(fs, "mss", mss);
     else
         fs_add_uint64(fs, "mss", 0);
