9bd5819108d36a8a7b8156b1db379d36f2248bfc
onursoyer <onrsyr@gmail.com>
2016-10-11 17:22:39 +0900

    bug searching

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -158,7 +158,7 @@ void synscan_process_packet(const u_char *packet,
         tcp_option_t* _opt = (tcp_option_t*)opt;
         if( _opt->kind == 1 /* NOP */ ) {
             ++opt;  // NOP is one byte;
-            print("[TEST] 4444444 \n");
+            printf("[TEST] 4444444 \n");
             continue;
         }
         if( _opt->kind == 2 /* MSS */ ) {
