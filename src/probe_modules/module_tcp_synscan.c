3aeb702b2a6a66465489114bf62f068468bb446a
onursoyer <onrsyr@gmail.com>
2016-10-12 10:24:22 +0900

    mss removed to check if there is a memory leak

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -152,28 +152,38 @@ void synscan_process_packet(const u_char *packet,
     uint16_t mss = 0;
 //    printf("[TEST] 111111 \n");
     uint8_t *tmp = (uint8_t * )tcp;
-//    if (tcp->th_off > 5) {
-//        uint8_t *opt = (uint8_t * )((char *)tmp + sizeof(struct tcphdr));
-////        printf("[TEST] 222222 \n");
-//        while (*opt != 0) {
-////            printf("[TEST] 33333333 \n");
-//            tcp_option_t *_opt = (tcp_option_t *) opt;
-//            if (_opt->kind == 1 /* NOP */ ) {
-//                ++opt;  // NOP is one byte;
-////                printf("[TEST] 4444444 \n");
-//                continue;
-//            }
-//            if (_opt->kind == 2 /* MSS */ ) {
-////                mss = ((*(opt + (sizeof(*_opt)))) << 8) + *(opt + sizeof(*_opt) + 1);
-////                unsigned int* mss_opt = (unsigned int*)(opt + sizeof(tcp_option_t));
-////                mss = htons(*mss_opt);
-//            }
-//            opt += _opt->size;
-////            printf("[TEST] 5555555 \n");
-//            if (_opt->size == 0)
-//                break;
-//        }
-//    }
+    if (tcp->th_off > 5) {
+        uint8_t *opt = (uint8_t * )((char *)tmp + sizeof(struct tcphdr));
+//        printf("[TEST] 222222 \n");
+        while (*opt != 0) {
+//            printf("[TEST] 33333333 \n");
+            tcp_option_t *_opt = (tcp_option_t *) opt;
+            if (_opt->kind == 1 /* NOP */ ) {
+                ++opt;  // NOP is one byte;
+//                printf("[TEST] 4444444 \n");
+                continue;
+            }
+            if (_opt->kind == 2 /* MSS */ ) {
+                if(_opt->size == 4){
+
+                }
+//                mss = ((*(opt + (sizeof(*_opt)))) << 8) + *(opt + sizeof(*_opt) + 1);
+//                unsigned int* mss_opt = (unsigned int*)(opt + sizeof(tcp_option_t));
+//                mss = htons(*mss_opt);
+            }
+            opt += _opt->size;
+//            printf("[TEST] 5555555 \n");
+            if (opt->kind == 1 /* NOP */ ) {
+                printf("[ERROR] _opt->kind : %d\n", opt->kind);
+            }
+
+            if (_opt->size == 0)
+                break;
+            }
+
+
+        }
+    }
     if (mss != 0)
         fs_add_uint64(fs, "mss", mss);
     else
