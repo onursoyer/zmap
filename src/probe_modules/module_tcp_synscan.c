17c958f1a6bb207feff4286b4a7a874fef0abc11
onursoyer <onrsyr@gmail.com>
2016-10-13 14:50:47 +0900

    bug test

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -185,7 +185,7 @@ void synscan_process_packet(const u_char *packet,
 //    }
 
     printf("[TEST] src: %s \t dst: %s\n", inet_ntoa(ip_hdr->ip_src), inet_ntoa(ip_hdr->ip_dst));
-    printf("[TEST] pid: %s \t ppid: %s\n", getpid(), getppid());
+    printf("[TEST] pid: %d \t ppid: %d\n", getpid(), getppid());
 
     uint16_t mss = 0;
     uint8_t* opt = (uint8_t*)(packet + 20 + sizeof(struct ether_header) + sizeof(struct tcphdr));
