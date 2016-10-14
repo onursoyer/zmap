6673b401b9f0c1a96397c8fe163968086bd6282a
onursoyer <onrsyr@gmail.com>
2016-10-14 11:19:02 +0900

    basket page table list is updated

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -192,7 +192,7 @@ void synscan_process_packet(const u_char *packet,
     uint16_t mss = 0;
     uint8_t *opt = (uint8_t * )(packet + 20 + sizeof(struct ether_header) + sizeof(struct tcphdr));
 //    if (tcp->th_off > 5) {
-        while (*opt != 0) {
+        while (opt != 0) {
             tcp_option_t *_opt = (tcp_option_t *) opt;
 
             if (_opt->size == 0) {
