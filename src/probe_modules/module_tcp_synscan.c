bdeb65a1efeafef5466f7f748fcbd11b1be62b22
onursoyer <onrsyr@gmail.com>
2016-10-14 10:59:52 +0900

    basket page table list is updated

--- src/probe_modules/module_tcp_synscan.c
+++ src/probe_modules/module_tcp_synscan.c
@@ -186,37 +186,39 @@ void synscan_process_packet(const u_char *packet,
 //        }
 //    }
 
-    printf("[TEST] src: %s \t dst: %s\n", inet_ntoa(ip_hdr->ip_src), inet_ntoa(ip_hdr->ip_dst));
+//    printf("[TEST] src: %s \t dst: %s\n", inet_ntoa(ip_hdr->ip_src), inet_ntoa(ip_hdr->ip_dst));
 //    printf("[TEST] pid: %d \t ppid: %d \t tid: %d\n", getpid(), getppid(), syscall(SYS_gettid));
 
     uint16_t mss = 0;
-    uint8_t* opt = (uint8_t*)(packet + 20 + sizeof(struct ether_header) + sizeof(struct tcphdr));
-    while( *opt != 0 ) {
-        tcp_option_t* _opt = (tcp_option_t*)opt;
+    uint8_t *opt = (uint8_t * )(packet + 20 + sizeof(struct ether_header) + sizeof(struct tcphdr));
+    if (tcp->th_off > 5) {
+        while (*opt != 0) {
+            tcp_option_t *_opt = (tcp_option_t *) opt;
 
-        if (_opt->size == 0) {
-            break;
-        }
+            if (_opt->size == 0) {
+                break;
+            }
 
-        if( _opt->kind == 1 /* NOP */ ) {
-            ++opt;  // NOP is one byte;
-            continue;
-        }
+            if (_opt->kind == 1 /* NOP */ ) {
+                ++opt;  // NOP is one byte;
+                continue;
+            }
 //        if( _opt->kind == 2 /* MSS */ ) {
 //            mss = ntohs((uint16_t)*(opt + sizeof(opt)));
 //            mss = ((*(opt + (sizeof(*_opt)))) << 8) + *(opt + sizeof(*_opt) + 1);
 //        }
 
-        if (_opt->kind == 0) {
-            printf("[MOBI] KIND : 0 ===== 0\n");
-        }
+            if (_opt->kind == 0) {
+                printf("[MOBI] KIND : 0 ===== 0\n");
+            }
 
-        opt += _opt->size;
-        if (_opt->kind == 0) {
-            printf("[MOBI] KIND : 0 ===== 1\n");
-        }
-        if (_opt->size == 0) {
-            break;
+            opt += _opt->size;
+            if (_opt->kind == 0) {
+                printf("[MOBI] KIND : 0 ===== 1\n");
+            }
+            if (_opt->size == 0) {
+                break;
+            }
         }
     }
 
